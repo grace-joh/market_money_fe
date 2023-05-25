ad60669d543d327756613164fe42f8c57c309726
Grace Joh <graceehjoh@gmail.com>
2023-05-25 09:21:53 -0500

    Add/test link to Markets show page

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Market Index Page' do
     @markets = MarketFacade.new.all_markets
   end
 
-  it 'displays all markets' do
+  it 'displays all markets with links' do
     visit markets_path
 
     expect(page).to have_content('Markets')
@@ -20,6 +20,17 @@ RSpec.describe 'Market Index Page' do
           expect(page).to have_button('More Info')
         end
       end
+    end
+
+    within('tr#market-322474') do
+      click_button('More Info')
+      expect(current_path).to eq(market_path(322474))
+    end
+
+    visit markets_path
+    within('tr#market-322458') do
+      click_button('More Info')
+      expect(current_path).to eq(market_path(322458))
     end
   end
 end
