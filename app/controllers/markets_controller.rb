dd65a7cad51ace12d2c3e2a6cc334b56264d12e8
Grace Joh <graceehjoh@gmail.com>
2023-05-25 09:26:32 -0500

    Add Markets controller show action

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -2,4 +2,8 @@ class MarketsController < ApplicationController
   def index
     @markets = MarketFacade.new.all_markets
   end
+
+  def show
+    @market = MarketFacade.new.market_with_vendors(params[:id])
+  end
 end
